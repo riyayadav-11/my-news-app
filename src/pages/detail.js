db50fd89bae5f5d5ccd6feac8e2d38ba4541663c
Neeraj Jangra <jangra.nirj2000@gmail.com>
2022-03-31 17:58:24 +0530

    css styling

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {useParams} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-export default function Detail({searchData}) {
-  const {id} = useParams();
-  const selectedArticle = searchData.filter((data, i) => i == id)?.[0];
-  console.log(selectedArticle)
+export default function Detail({ searchData }) {
+  const { id } = useParams();
+  const selectedArticle = searchData.filter((data, i) => i === id)?.[0];
+  console.log(selectedArticle);
   return (
-    <div style={{margin: "20px"}}>
-      <Link to="/">
-        Back
-      </Link>
-      <h2>
-        {selectedArticle.title}
-      </h2>
-      <p>
-        {selectedArticle.content}
-      </p>
+    <div className="newsContainer">
+      <div className="newsContent" style={{ margin: "20px" }}>
+        <Link to="/">Back</Link>
+        <h2>{selectedArticle.title}</h2>
+        <p>{selectedArticle.content}</p>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Home({searchData, searchString, setSearchString}) {
+export default function Home({ searchData, searchString, setSearchString }) {
   return (
     <div>
-      <div className='search-input'>
-        <input placeholder="Search a headline" value={searchString} onChange={e => setSearchString(e.target.value)} />
-        <button>SEARCH</button>
+      <div className="search-input">
+        <input
+          placeholder="Search a headline"
+          value={searchString}
+          onChange={(e) => setSearchString(e.target.value)}
+        />
+        <button>Search</button>
       </div>
-      {searchData?.map((data, i) => (
-        <div key={i}>
-          <Link to={`/${i}`}>
-            <div className='news-link'>
-              <h2>{data.title}</h2>
-              <p>{data.description}</p>
-            </div>
-          </Link>
-        </div>
-      ))}
+
+      <div className="newsContainer">
+        {searchData?.map((data, i) => (
+          <div className="news-link" key={i}>
+            <Link to={`/${i}`}>
+              <div >
+                <h2>{data.title}</h2>
+                <p>{data.description}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
